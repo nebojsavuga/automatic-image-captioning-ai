598771f5f8168964ff49edd4b5cc2bec21276fba
BogdanJ33 <bogdanjanosevic@gmail.com>
2024-12-16 22:20:46 +0100

    Added pretprocess all images

--- backend/preprocess.py
+++ backend/preprocess.py
@@ -1,3 +1,4 @@
+import os
 import re
 import numpy as np
 from keras.preprocessing.image import load_img, img_to_array
@@ -30,7 +31,19 @@ def get_captions_with_file_names(path):
     )
     return captions
 
+
+def preprocess_all_images(folder_path):
+    preprocessed_images = []
+    
+    for filename in os.listdir(folder_path):
+        if filename.endswith(('.jpg', '.jpeg', '.png')):
+            image_path = os.path.join(folder_path, filename)  
+            preprocessed_image = preprocess_image(image_path) 
+            preprocessed_images.append(preprocessed_image)  
+    return preprocessed_images
+
 captions = get_captions_with_file_names("images/results.csv")
-print(captions)
-captions["processed_caption"] = captions["caption"].apply(preprocess_text)
-print(captions)
+captions["caption"] = captions["caption"].apply(preprocess_text)
+folder_path = "images/flickr30k_images"  
+all_preprocessed_images = preprocess_all_images(folder_path)
+print(f"Ukupno preprocesiranih slika: {len(all_preprocessed_images)}")
