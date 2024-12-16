d4e0082d8d04f43ee26a838628e967000c02e9d9
BogdanJ33 <bogdanjanosevic@gmail.com>
2024-12-16 19:50:52 +0100

    Changed preprocess text a little

--- backend/preprocess.py
+++ backend/preprocess.py
@@ -5,63 +5,15 @@ from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.preprocessing.image import load_img, img_to_array
 import pandas as pd
 
-stop_words = [
-    "a",
-    "an",
-    "the",
-    "and",
-    "or",
-    "but",
-    "if",
-    "is",
-    "are",
-    "was",
-    "were",
-    "in",
-    "on",
-    "at",
-    "by",
-    "for",
-    "from",
-    "of",
-    "to",
-    "with",
-    "it",
-    "its",
-    "this",
-    "that",
-    "these",
-    "those",
-    "he",
-    "she",
-    "they",
-    "we",
-    "you",
-    "i",
-    "me",
-    "my",
-    "your",
-    "our",
-    "their",
-    "be",
-    "been",
-    "being",
-    "do",
-    "did",
-    "does",
-    "have",
-    "has",
-    "having",
-    "not",
-]
-
 
 def preprocess_text(text):
+    if not isinstance(text, str):
+        return ""  
     text = re.sub(r"[^\w\s]", "", text)
     text = text.lower()
+    text = text.replace('\s+', ' ')
     words = text.split()
-    words = [word for word in words if word not in stop_words]
-    return "start " + " ".join(words) + " end"
+    return "startseq " + " ".join(words) + " endseq"
 
 
 def preprocess_image(image_path):
@@ -79,3 +31,6 @@ def get_captions_with_file_names(path):
     return captions
 
 captions = get_captions_with_file_names("images/results.csv")
+print(captions)
+captions["processed_caption"] = captions["caption"].apply(preprocess_text)
+print(captions)
