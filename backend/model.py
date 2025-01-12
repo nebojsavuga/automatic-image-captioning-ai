eed392dc5620d5c8cad3012ec10b2fb4bed3d027
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-01-12 11:20:02 +0100

    added rnn model

---
+++ backend/model.py
@@ -0,0 +1,20 @@
+from keras.models import Model
+from keras.layers import Input, LSTM, Embedding, Dense, Dropout, add
+
+
+def get_model(max_length, vocabulary_size):
+    inputs1 = Input(shape=(4096,))
+    fe1 = Dropout(0.4)(inputs1)
+    fe2 = Dense(256, activation="relu")(fe1)
+    inputs2 = Input(shape=(max_length,))
+    se1 = Embedding(vocabulary_size, 256, mask_zero=True)(inputs2)
+    se2 = Dropout(0.4)(se1)
+    se3 = LSTM(256)(se2)
+
+    # decoder
+    decoder1 = add([fe2, se3])
+    decoder2 = Dense(256, activation="relu")(decoder1)
+    outputs = Dense(vocabulary_size, activation="softmax")(decoder2)
+
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+    model.compile(loss="categorical_crossentropy", optimizer="adam")
